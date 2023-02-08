6b3bf708558f15ff5a0c9e323d931685346fafcf
Shaurya Vardhan <shaurya.vardhan.9758@gmail.com>
2023-02-08 19:54:34 +0530

    recursion

--- Recursion/a_pow_b.cpp
+++ Recursion/a_pow_b.cpp
@@ -1,17 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int solve(int a,int b)
+int solve(int a,int b) // T(N) = O(b)
 {
     if(b==0)
     {
         return 1;
     }
+    if(b==1)
+    {
+        return a;
+    }
     return a * solve(a, b - 1);
+}
+int solve2(int a,int b)// T(N)=O(log(b))
+{
+    if(b==1)
+    {
+        return a;
+    }
+    int temp = solve2(a, b / 2);
+    if(b%2==0)
+    {
+        return temp * temp;
+    }
+    else
+    {
+        return a * temp * temp;
+    }
 }
 int main()
 {
     int s;
-    s = solve(3,1);
+    s = solve2(3,3);
     cout << s << endl;
     return 0;
 }
